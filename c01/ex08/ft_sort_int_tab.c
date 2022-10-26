2af353722d7b4168707845ec6aa96dd2cc8ab030
Simone Tartaglia <startagl@e3r4p2.42roma.it>
2022-10-26 14:08:33 +0200

    c01

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: startagl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 12:48:40 by startagl          #+#    #+#             */
+/*   Updated: 2022/10/19 14:10:09 by startagl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	temp = 0;
+	i = 0;
+	while (i <= size)
+	{
+		j = i;
+		while (j < size)
+		{
+			if (tab [j] > tab[j + 1])
+			{
+				temp = tab[j + 1];
+				tab[j + 1] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		size --;
+	}
+}
